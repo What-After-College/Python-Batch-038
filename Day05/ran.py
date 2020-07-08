69382e44c85a5aa3ca1db064a94785855adef8e3
akannshu <akanshuraj@gmail.com>
2020-07-08 13:46:25 +0530

    Added Day05 Codes

---
+++ Day05/ran.py
@@ -0,0 +1,25 @@
+import random
+
+# k = random.randint(1,100)
+# print(k)
+
+"""
+Akanshu -> 48
+Sahil -> 87
+Argha -> 56
+Aman -> 23
+Ankita -> 94
+Ambika -> 44
+Pooja -> 33
+Ayusha -> 66
+Saquib -> 29
+Kajal -> 88
+Rhythm -> 75
+Deepti -> 82
+Srijita -> 45
+"""
+
+names = ["Akanshu","Sahil","Argha", "Aman", "Ankita", "Ambika", "Pooja", "Ayusha", "Saquib", "Kajal", "Rhythm", "Deepti", "Srijita"]
+
+k = random.randint(0,len(names)-1)
+print(names[k])
