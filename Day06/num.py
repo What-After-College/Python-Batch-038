301ca4d6828812d65ebd60480395985cf64ad2a9
akannshu <akanshuraj@gmail.com>
2020-07-10 13:26:09 +0530

    Added Day06

---
+++ Day06/num.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+array = np.array([1,2,3,4])
+# print(array)
+# print(type(array))
+
+# print(array.shape)
+
+# print(array[0], array[1], array[2])
+# print(array[-1])
+
+# arr2d = np.array([[1,2,3],[4,5,6]])
+# print(arr2d)
+# print(arr2d.shape)
+
+# arr = np.full((4,5),987)
+# print(arr)
+
+
+x = np.array([[1,2],[3,4]])
+y = np.array([[5,6],[7,8]])
+
+print(np.dot(x,y))
